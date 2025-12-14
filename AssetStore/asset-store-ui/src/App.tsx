96b823b2e74f10a6b43f83a263c840be74ed56e3
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2025-12-14 17:05:58 +0200

    #52 tsx.css

--- AssetStore/asset-store-ui/src/App.tsx
+++ AssetStore/asset-store-ui/src/App.tsx
@@ -2,6 +2,8 @@ import { Header } from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from "./pages/HomePage";
 
+import './App.tsx.css';
+
 export default function App() {
   return (
     <Router>
