bbc57edb8ee91ff6b761f8955c9360b7daee9634
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2026-06-20 19:48:07 +0300

    #109 Fix windowing

--- SharpEngine.Core/Renderers/ParticleRenderer.cs
+++ SharpEngine.Core/Renderers/ParticleRenderer.cs
@@ -1,4 +1,7 @@
-﻿using SharpEngine.Core.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using SharpEngine.Core.Entities.Views;
+using SharpEngine.Core.Interfaces;
+using SharpEngine.Core.Scenes;
 using System;
 using System.Threading.Tasks;
 
@@ -17,9 +20,27 @@ public class ParticleRenderer : RendererBase
     {
     }
 
+    /// <summary>
+    ///     Initializes a new instance of <see cref="UIRenderer"/>.
+    /// </summary>
+    public ParticleRenderer(CameraView camera, ISettings settings, Scene scene)
+        : this(camera, settings, scene, LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<UIRenderer>())
+    {
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of <see cref="UIRenderer"/>.
+    /// </summary>
+    public ParticleRenderer(CameraView camera, ISettings settings, Scene scene, ILogger<UIRenderer> logger) : base(settings)
+    {
+        // _scene = scene;
+        // _camera = camera;
+        // _logger = logger;
+    }
+
     /// <inheritdoc />
     public override RenderFlags RenderFlag => RenderFlags.All;
 
     /// <inheritdoc />
-    public override Task Render() => throw new NotImplementedException();
+    public override Task Render() => Task.CompletedTask; // => throw new NotImplementedException();
 }
