f6a7e9d18f2e100183a6cfaf133744682f7dd503
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2026-09-21 00:55:54 +0300

    Update NuGet packages

--- SharpEngine.Core.ObjLoader/Loaders/ObjLoader/ObjLoaderFactory.cs
+++ SharpEngine.Core.ObjLoader/Loaders/ObjLoader/ObjLoaderFactory.cs
@@ -12,98 +12,97 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions;
 
-namespace SharpEngine.Core.ObjLoader.Loaders.ObjLoader
+namespace SharpEngine.Core.ObjLoader.Loaders.ObjLoader;
+
+/// <summary>
+///     Handles loading 3D models.
+/// </summary>
+public static class ObjLoaderFactory
 {
-    /// <summary>
-    ///     Handles loading 3D models.
-    /// </summary>
-    public static class ObjLoaderFactory
+    private static readonly FileSystem _fileSystem;
+    private static readonly FileManager _fileManager;
+
+    private readonly static ILogger _logger = LoggingExtensions.CreateLogger(typeof(ObjLoaderFactory));
+
+    static ObjLoaderFactory()
     {
-        private static readonly FileSystem _fileSystem;
-        private static readonly FileManager _fileManager;
+        _fileSystem = new FileSystem();
+        _fileManager = new FileManager();
+    }
 
-        private readonly static ILogger _logger = LoggingExtensions.CreateLogger(typeof(ObjLoaderFactory));
+    const string FbxExtension = ".fbx";
+    const string ObjExtension = ".obj";
 
-        static ObjLoaderFactory()
+    /// <summary>
+    ///     Loads a mesh based on the file extension of the provided path.
+    /// </summary>
+    /// <param name="gl">The OpenGL context where the model should be bound.</param>
+    /// <param name="path">Specifies the file path of the mesh to be loaded, which determines the loading method based on its extension.</param>
+    /// <param name="model">The model loaded from the file.</param>
+    /// <returns>The model loaded from the file.</returns>
+    /// <exception cref="NotSupportedException">Thrown when the file extension of the provided path is not recognized as a supported mesh format.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when no meshes are loaded.</exception>
+    public static bool Load(GL gl, string path, out Model? model)
+    {
+        try
         {
-            _fileSystem = new FileSystem();
-            _fileManager = new FileManager();
-        }
-
-        const string FbxExtension = ".fbx";
-        const string ObjExtension = ".obj";
-
-        /// <summary>
-        ///     Loads a mesh based on the file extension of the provided path.
-        /// </summary>
-        /// <param name="gl">The OpenGL context where the model should be bound.</param>
-        /// <param name="path">Specifies the file path of the mesh to be loaded, which determines the loading method based on its extension.</param>
-        /// <param name="model">The model loaded from the file.</param>
-        /// <returns>The model loaded from the file.</returns>
-        /// <exception cref="NotSupportedException">Thrown when the file extension of the provided path is not recognized as a supported mesh format.</exception>
-        /// <exception cref="InvalidOperationException">Thrown when no meshes are loaded.</exception>
-        public static bool Load(GL gl, string path, out Model? model)
-        {
-            try
+            string fileExtension = Path.GetExtension(path);
+            model = fileExtension switch
             {
-                string fileExtension = Path.GetExtension(path);
-                model = fileExtension switch
-                {
-                    FbxExtension => LoadFbx("", path),
-                    ObjExtension => LoadObj(gl, path),
-                    _ => throw new NotSupportedException($"The file extension {fileExtension} is not a supported mesh file.")
-                };
+                FbxExtension => LoadFbx("", path),
+                ObjExtension => LoadObj(gl, path),
+                _ => throw new NotSupportedException($"The file extension {fileExtension} is not a supported mesh file.")
+            };
 
-                _logger.LogDebug("Successfully loaded model '{name}' from path: {path}.", model.Name, path);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogWarning(ex, "Failed to load model from path: {path}.", path);
-
-                model = null;
-                return false;
-            }
+            _logger.LogDebug("Successfully loaded model '{name}' from path: {path}.", model.Name, path);
+            return true;
         }
-
-        // TODO: #3 Load fbx mesh from file
-        private static Model LoadFbx(string identifier, string meshFilePath)
+        catch (Exception ex)
         {
-            throw new NotImplementedException();
+            _logger.LogWarning(ex, "Failed to load model from path: {path}.", path);
+
+            model = null;
+            return false;
         }
+    }
 
-        private static Model LoadObj(GL gl, string path)
-        {
-            var meshes = LoadObjMeshes(gl, path);
+    // TODO: #3 Load fbx mesh from file
+    private static Model LoadFbx(string identifier, string meshFilePath)
+    {
+        throw new NotImplementedException();
+    }
 
-            if (meshes.Count == 0)
-                throw new InvalidOperationException("The .obj file contains no meshes.");
+    private static Model LoadObj(GL gl, string path)
+    {
+        var meshes = LoadObjMeshes(gl, path);
 
-            return new Model(gl, path, meshes);
-        }
+        if (meshes.Count == 0)
+            throw new InvalidOperationException("The .obj file contains no meshes.");
 
-        private static List<Mesh> LoadObjMeshes(GL gl, string path)
-        {
-            var dataStore = new DataStore();
+        return new Model(gl, path, meshes);
+    }
 
-            var faceParser = new FaceParser(dataStore);
-            var groupParser = new GroupParser(dataStore);
-            var normalParser = new NormalParser(dataStore);
-            var textureParser = new TextureParser(dataStore);
-            var vertexParser = new VertexParser(dataStore);
-            var smoothingGroupParser = new SmoothingGroupParser(dataStore);
-            var objectNameParser = new ObjectNameParser(dataStore);
+    private static List<Mesh> LoadObjMeshes(GL gl, string path)
+    {
+        var dataStore = new DataStore();
 
-            var materialLibraryLoader = new MaterialLibraryLoader(dataStore, _fileSystem.FileStream, _fileManager);
+        var faceParser = new FaceParser(dataStore);
+        var groupParser = new GroupParser(dataStore);
+        var normalParser = new NormalParser(dataStore);
+        var textureParser = new TextureParser(dataStore);
+        var vertexParser = new VertexParser(dataStore);
+        var smoothingGroupParser = new SmoothingGroupParser(dataStore);
+        var objectNameParser = new ObjectNameParser(dataStore);
 
-            var materialLoader = new MaterialLibraryLoaderFacade(materialLibraryLoader, path);
-            var materialLibraryParser = new MaterialLibraryParser(materialLoader);
-            var useMaterialParser = new UseMaterialParser(dataStore);
+        var materialLibraryLoader = new MaterialLibraryLoader(dataStore, _fileSystem.FileStream, _fileManager);
 
-            var loader = new ObjLoader(path, dataStore, _fileSystem.FileStream, _fileManager)
-                .SetupTypeParsers(faceParser, groupParser, normalParser, textureParser, vertexParser, materialLibraryParser, useMaterialParser, smoothingGroupParser, objectNameParser);
+        var materialLoader = new MaterialLibraryLoaderFacade(materialLibraryLoader, path);
+        var materialLibraryParser = new MaterialLibraryParser(materialLoader);
+        var useMaterialParser = new UseMaterialParser(dataStore);
 
-            return loader.Load(gl);
-        }
+        var loader = new ObjLoader(path, dataStore, _fileSystem.FileStream, _fileManager)
+            .SetupTypeParsers(faceParser, groupParser, normalParser, textureParser, vertexParser, materialLibraryParser, useMaterialParser, smoothingGroupParser, objectNameParser);
+
+        return loader.Load(gl);
     }
 }
--- Tests/ObjLoader.Test/TypeParsers/GroupParserTests.cs
+++ Tests/ObjLoader.Test/TypeParsers/GroupParserTests.cs
@@ -4,55 +4,54 @@ using Xunit;
 using SharpEngine.Core.Components.ObjLoader.DataStore;
 using SharpEngine.Core.ObjLoader.Loader.TypeParsers;
 
-namespace SharpEngine.Core.ObjLoader.Tests.TypeParsers
+namespace SharpEngine.Core.ObjLoader.Tests.TypeParsers;
+
+public class GroupParserTests
 {
-    public class GroupParserTests
+    private readonly GroupDataStoreMock _groupDataStoreMock;
+    private readonly GroupParser _groupParser;
+
+    public GroupParserTests()
     {
-        private readonly GroupDataStoreMock _groupDataStoreMock;
-        private readonly GroupParser _groupParser;
-
-        public GroupParserTests()
-        {
-            _groupDataStoreMock = new GroupDataStoreMock();
-            _groupParser = new GroupParser(_groupDataStoreMock);
-        }
-
-        [Fact]
-        public void CanParse_returns_true_on_normal_line()
-        {
-            const string groupKeyword = "g";
-
-            bool canParse = _groupParser.CanParse(groupKeyword);
-            canParse.Should().BeTrue();
-        }
-
-        [Fact]
-        public void CanParse_returns_false_on_non_normal_line()
-        {
-            const string invalidKeyword = "vt";
-
-            bool canParse = _groupParser.CanParse(invalidKeyword);
-            canParse.Should().BeFalse();
-        }
-
-        [Fact]
-        public void Parses_normal_line_correctly()
-        {
-            const string normalLine = "test group";
-            _groupParser.Parse(normalLine);
-
-            var parsedGroupName = _groupDataStoreMock.ParsedGroupName;
-            parsedGroupName.Should().Be("test group");
-        }
+        _groupDataStoreMock = new GroupDataStoreMock();
+        _groupParser = new GroupParser(_groupDataStoreMock);
     }
 
-    class GroupDataStoreMock : IGroupDataStore
+    [Fact]
+    public void CanParse_returns_true_on_normal_line()
     {
-        public string ParsedGroupName { get; set; }
-        
-        public void PushGroup(string groupName)
-        {
-            ParsedGroupName = groupName;
-        }
+        const string groupKeyword = "g";
+
+        bool canParse = _groupParser.CanParse(groupKeyword);
+        canParse.Should().BeTrue();
+    }
+
+    [Fact]
+    public void CanParse_returns_false_on_non_normal_line()
+    {
+        const string invalidKeyword = "vt";
+
+        bool canParse = _groupParser.CanParse(invalidKeyword);
+        canParse.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Parses_normal_line_correctly()
+    {
+        const string normalLine = "test group";
+        _groupParser.Parse(normalLine);
+
+        var parsedGroupName = _groupDataStoreMock.ParsedGroupName;
+        parsedGroupName.Should().Be("test group");
+    }
+}
+
+class GroupDataStoreMock : IGroupDataStore
+{
+    public string ParsedGroupName { get; set; }
+    
+    public void PushGroup(string groupName)
+    {
+        ParsedGroupName = groupName;
     }
 }
